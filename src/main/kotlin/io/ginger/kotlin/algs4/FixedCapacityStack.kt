53fa3ba30e86e5ea36ce20e30c89902df7899f04
吴凡 <386534960@qq.com>
2017-06-08 22:55:25 +0800

    make fixedcapacity stack implement iterable

--- src/main/kotlin/io/ginger/kotlin/algs4/FixedCapacityStack.kt
+++ src/main/kotlin/io/ginger/kotlin/algs4/FixedCapacityStack.kt
@@ -3,21 +3,21 @@ package io.ginger.kotlin.algs4
 /**
  * Created by wufan on 2017/6/8.
  */
-class FixedCapacityStack<T>(cap: Int) {
+class FixedCapacityStack<T>(cap: Int) : Iterable<T> {
     @Suppress("UNCHECKED_CAST")
-    private val a: Array<T> = Array(cap, {Any()}) as Array<T>
+    private val a: Array<T> = Array(cap, { Any() }) as Array<T>
     var size: Int = 0
         get() = field
 
     fun push(item: T) {
-        if(size == a.size) {
+        if (size == a.size) {
             throw IndexOutOfBoundsException()
         }
         a[size++] = item
     }
 
     fun pop(): T {
-        if(size == 0) {
+        if (size == 0) {
             throw IndexOutOfBoundsException()
         }
         return a[--size]
@@ -26,15 +26,34 @@ class FixedCapacityStack<T>(cap: Int) {
     fun isEmpty(): Boolean {
         return size == 0
     }
+
+    override fun iterator(): Iterator<T> {
+        return FixedCapacityStackIterator(size)
+    }
+
+    private inner class FixedCapacityStackIterator(var current: Int): Iterator<T> {
+
+        override fun hasNext(): Boolean {
+            return current != 0
+        }
+
+        override fun next(): T {
+            return a[--current]
+        }
+
+    }
+
 }
 
 fun main(args: Array<String>) {
     val stack = FixedCapacityStack<String>(100)
     val stdin = StdIn()
-    while(!stdin.isEmpty()) {
+    while (!stdin.isEmpty()) {
         val s = stdin.readString()
-        if(s != "-") stack.push(s)
-        else if(!s.isEmpty()) print("${stack.pop()} ")
+        if (s != "-") stack.push(s)
+        else if (!s.isEmpty()) print("${stack.pop()} ")
     }
     println("(${stack.size} left on stack)")
+
+    stack.forEach({println(it)})
 }
